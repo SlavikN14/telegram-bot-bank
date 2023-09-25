adc1bf577b1a0d25d74d90043171b65305f6fc1b
slavikn <slavir186@gmail.com>
2023-09-25 17:26:19 +0300

    add currency repository

---
+++ src/main/kotlin/com/ajaxproject/telegrambot/bot/repository/CurrencyExchangeRepositoryImpl.kt
@@ -0,0 +1,41 @@
+package com.ajaxproject.telegrambot.bot.repository
+
+import com.ajaxproject.telegrambot.bot.enums.Currency
+import com.ajaxproject.telegrambot.bot.models.currency.MongoCurrency
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.stereotype.Repository
+
+@Repository
+class CurrencyExchangeRepositoryImpl(
+    private val mongoTemplate: MongoTemplate,
+) : CurrencyExchangeRepository {
+    override fun findByCode(code: Int): List<MongoCurrency> {
+        val query: Query = Query().addCriteria(
+            Criteria.where("currency_code_A").`is`(code)
+                .orOperator(
+                    Criteria.where("currency_code_B").`is`(Currency.UAH.code),
+                    Criteria.where("currency_code_B").`is`(Currency.USD.code),
+                    Criteria.where("currency_code_B").`is`(Currency.EUR.code)
+                )
+        )
+        return mongoTemplate.find(query, MongoCurrency::class.java, MongoCurrency.COLLECTION_NAME)
+    }
+
+    override fun findAll(): List<MongoCurrency> =
+        mongoTemplate.findAll(MongoCurrency::class.java, MongoCurrency.COLLECTION_NAME)
+
+    override fun save(currency: MongoCurrency): MongoCurrency {
+        return mongoTemplate.save(currency, MongoCurrency.COLLECTION_NAME)
+    }
+
+    override fun deleteById(userId: ObjectId) {
+        mongoTemplate.findAndRemove(
+            Query(Criteria.where("id").`is`(userId)),
+            MongoCurrency::class.java,
+            MongoCurrency.COLLECTION_NAME
+        )
+    }
+}
