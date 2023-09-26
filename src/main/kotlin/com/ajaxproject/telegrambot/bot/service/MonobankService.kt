48f9e8fe4651953542ba895c5ff7152373d27167
slavikn <slavir186@gmail.com>
2023-09-26 18:17:21 +0300

    fix problems with http response

--- src/main/kotlin/com/ajaxproject/telegrambot/bot/service/MonobankService.kt
+++ src/main/kotlin/com/ajaxproject/telegrambot/bot/service/MonobankService.kt
@@ -4,11 +4,11 @@ import com.ajaxproject.telegrambot.bot.dto.MonobankCurrencyExchangeResponse
 import com.ajaxproject.telegrambot.bot.properties.MonobankProperties
 import com.ajaxproject.telegrambot.bot.utils.HttpUtils
 import com.ajaxproject.telegrambot.bot.utils.JsonUtils
-import com.squareup.okhttp.Request
-import com.squareup.okhttp.Response
+import okhttp3.Request
+import okhttp3.Response
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
-
 
 @Service
 class MonobankService(
@@ -16,24 +16,30 @@ class MonobankService(
     val currencyExchangeService: CurrencyExchangeService,
 ) {
 
-    private final val request: Request = Request.Builder().url(urlProperties.url).get().build()
-
-    val response: Response = HttpUtils.HTTP_CLIENT.newCall(request).execute()
+    private val request: Request = Request.Builder().url(urlProperties.url).get().build()
 
     fun getCurrencyExchangeRates(): Array<MonobankCurrencyExchangeResponse> {
+        val response: Response = HttpUtils.HTTP_CLIENT
+            .newCall(request).execute()
+        var arrayResponses = emptyArray<MonobankCurrencyExchangeResponse>()
         if (response.isSuccessful) {
-            return JsonUtils.GSON.fromJson(
-                response.body().string(), Array<MonobankCurrencyExchangeResponse>::class.java
+            arrayResponses = JsonUtils.GSON.fromJson(
+                response.body?.string(), Array<MonobankCurrencyExchangeResponse>::class.java
             )
+            response.body?.close()
         } else {
-            throw ResponseIsFailedException("Failed response")
+            log.error("HTTP request failed with code {} and response body: {}", response.code, response.body?.string())
         }
+        return arrayResponses
     }
 
     @Scheduled(fixedRate = 300000)
     fun sendCurrencyExchangeRates() {
         currencyExchangeService.addAllCurrency(getCurrencyExchangeRates())
+        log.info("Update data in database")
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(MonobankService::class.java)
     }
 }
-
-class ResponseIsFailedException(message: String) : Exception(message)
