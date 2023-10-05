5856febf895545d7e62c03b70c8644df2f67a4b1
slavikn <slavir186@gmail.com>
2023-10-05 18:52:54 +0300

    add enum classes

--- src/main/kotlin/com/ajaxproject/telegrambot/bot/enums/Currency.kt
+++ src/main/kotlin/com/ajaxproject/telegrambot/bot/enums/Currency.kt
@@ -1,11 +1,10 @@
 package com.ajaxproject.telegrambot.bot.enums
 
 enum class Currency(
-    val symbol: String,
     val code: Int,
 ) {
 
-    UAH("₴", code = 980),
-    USD("$", code = 840),
-    EUR("€", code = 978)
+    UAH(code = 980),
+    USD(code = 840),
+    EUR(code = 978)
 }
