e17d314d4df035908436f16b069afaeb857a1cba
slavikn <slavir186@gmail.com>
2023-10-06 12:16:30 +0300

    Combining two entities: MongoIncome and MongoExpense into one MongoFinance

--- src/main/kotlin/com/ajaxproject/telegrambot/bot/models/MongoFinance.kt
+++ src/main/kotlin/com/ajaxproject/telegrambot/bot/models/MongoFinance.kt
@@ -1,24 +1,26 @@
 package com.ajaxproject.telegrambot.bot.models
 
-import com.ajaxproject.telegrambot.bot.models.MongoExpense.Companion.COLLECTION_NAME
+import com.ajaxproject.telegrambot.bot.models.MongoFinance.Companion.COLLECTION_NAME
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.TypeAlias
 import org.springframework.data.mongodb.core.mapping.Document
 import java.util.*
 
-@TypeAlias("Expenses")
+@TypeAlias("Finances")
 @Document(value = COLLECTION_NAME)
-data class MongoExpense(
+data class MongoFinance(
+
     @Id
     val id: ObjectId,
     val userId: Long,
+    val financeType: String,
     val amount: Double,
     val description: String,
     val date: Date = Date(),
 ) {
 
     companion object {
-        const val COLLECTION_NAME = "expenses"
+        const val COLLECTION_NAME = "finances"
     }
 }
