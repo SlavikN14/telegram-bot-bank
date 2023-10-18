91aeb654963d3fb3a6cb9963c2c78a06bc882a56
slavikn <slavir186@gmail.com>
2023-10-18 13:02:39 +0300

    simplification of logic to avoid DRY

--- telegram-bot-api/src/main/kotlin/com/ajaxproject/telegrambot/bot/handlers/impl/FinanceHandler.kt
+++ telegram-bot-api/src/main/kotlin/com/ajaxproject/telegrambot/bot/handlers/impl/FinanceHandler.kt
@@ -1,18 +1,22 @@
 package com.ajaxproject.telegrambot.bot.handlers.impl
 
-import com.ajaxproject.telegrambot.bot.enums.Commands.ADD_INCOME
+import com.ajaxproject.financeservice.dto.toFinanceResponse
+import com.ajaxproject.telegrambot.bot.enums.Commands.GET_EXPENSES
+import com.ajaxproject.telegrambot.bot.enums.Commands.ADD_FINANCE
 import com.ajaxproject.telegrambot.bot.enums.Commands.GET_INCOMES
 import com.ajaxproject.telegrambot.bot.enums.Commands.MENU
 import com.ajaxproject.telegrambot.bot.enums.ConversationState.CONVERSATION_STARTED
-import com.ajaxproject.telegrambot.bot.enums.ConversationState.WAITING_FOR_ADD_INCOME
-import com.ajaxproject.telegrambot.bot.enums.Finance.INCOME
-import com.ajaxproject.telegrambot.bot.enums.TextPropertyName.ADD_FINANCE
+import com.ajaxproject.telegrambot.bot.enums.ConversationState.WAITING_FOR_ADD_FINANCE
+import com.ajaxproject.financeservice.enums.Finance
+import com.ajaxproject.telegrambot.bot.enums.TextPropertyName.ADD_FINANCE_TEXT
 import com.ajaxproject.telegrambot.bot.enums.TextPropertyName.BACK_TO_MENU
 import com.ajaxproject.telegrambot.bot.enums.TextPropertyName.FAILED_ADD_FINANCE
 import com.ajaxproject.telegrambot.bot.enums.TextPropertyName.SUCCESSFUL_ADD_FINANCE
 import com.ajaxproject.telegrambot.bot.handlers.UserRequestHandler
-import com.ajaxproject.telegrambot.bot.models.MongoFinance
-import com.ajaxproject.telegrambot.bot.service.FinanceService
+import com.ajaxproject.financeservice.models.MongoFinance
+import com.ajaxproject.financeservice.enums.Finance.INCOME
+import com.ajaxproject.financeservice.enums.Finance.EXPENSE
+import com.ajaxproject.telegrambot.bot.service.FinanceRequestNatsService
 import com.ajaxproject.telegrambot.bot.service.TelegramService
 import com.ajaxproject.telegrambot.bot.service.TextService
 import com.ajaxproject.telegrambot.bot.service.UserSessionService
@@ -20,61 +24,62 @@ import com.ajaxproject.telegrambot.bot.service.isTextMessage
 import com.ajaxproject.telegrambot.bot.service.updatemodels.UpdateRequest
 import com.ajaxproject.telegrambot.bot.service.updatemodels.UpdateSession
 import com.ajaxproject.telegrambot.bot.utils.KeyboardUtils
-import org.bson.types.ObjectId
 import org.springframework.stereotype.Component
+import java.util.*
 
 @Component
-class AddIncomesButtonHandler(
+class AddFinancesButtonHandler(
     private val telegramService: TelegramService,
     private val textService: TextService,
     private val userSessionService: UserSessionService,
 ) : UserRequestHandler {
 
     override fun isApplicable(request: UpdateRequest): Boolean {
-        return isCommand(request.update, ADD_INCOME.command)
+        return isCommand(request.update, ADD_FINANCE.command)
     }
 
     override fun handle(dispatchRequest: UpdateRequest) {
         telegramService.sendMessage(
             chatId = dispatchRequest.chatId,
-            text = textService.readText(ADD_FINANCE.name)
+            text = textService.readText(ADD_FINANCE_TEXT.name)
         )
         val session: UpdateSession = dispatchRequest.updateSession.apply {
-            state = WAITING_FOR_ADD_INCOME
+            state = WAITING_FOR_ADD_FINANCE
         }
         userSessionService.saveSession(dispatchRequest.chatId, session)
     }
 }
 
 @Component
-class AddIncomesHandler(
+class AddFinanceHandler(
     private val telegramService: TelegramService,
-    private val financeService: FinanceService,
+    private val financeRequestNatsService: FinanceRequestNatsService,
     private val textService: TextService,
     private val userSessionService: UserSessionService,
 ) : UserRequestHandler {
 
     override fun isApplicable(request: UpdateRequest): Boolean {
-        return WAITING_FOR_ADD_INCOME == request.updateSession.state &&
-            request.update.isTextMessage()
+        return WAITING_FOR_ADD_FINANCE == request.updateSession.state &&
+                request.update.isTextMessage()
     }
 
     override fun handle(dispatchRequest: UpdateRequest) {
-        val income = dispatchRequest.update.message.text
+        val financeData = dispatchRequest.update.message.text
         val chatId = dispatchRequest.chatId
+        val financeType = checkDataIncomeOrExpense(financeData)
 
-        if (!income.contains(":")) {
+        if (!financeData.matches(Regex("[+-]\\d+\\s+\\w+"))) {
             telegramService.sendMessage(chatId, textService.readText(FAILED_ADD_FINANCE.name))
             return
         }
 
-        financeService.addFinance(
+        financeRequestNatsService.requestToCreateFinance(
             MongoFinance(
-                id = ObjectId(),
                 userId = dispatchRequest.chatId,
-                financeType = INCOME,
-                amount = income.split(":")[0].toDouble(),
-                description = income.split(":")[1]
+                financeType = financeType,
+                amount = financeData.substring(1).split(" ")[0].toDouble(),
+                description = financeData.split(" ")[1],
+                date = Date()
             )
         )
 
@@ -84,7 +89,7 @@ class AddIncomesHandler(
             replyKeyboard = KeyboardUtils.run {
                 inlineKeyboard(
                     inlineRowKeyboard(
-                        inlineButton("Add income again", ADD_INCOME.command),
+                        inlineButton("Add finance again", ADD_FINANCE.command),
                         inlineButton("Return to menu", MENU.command)
                     )
                 )
@@ -95,22 +100,34 @@ class AddIncomesHandler(
         }
         userSessionService.saveSession(dispatchRequest.chatId, session)
     }
+
+    private fun checkDataIncomeOrExpense(financeData: String): Finance {
+        return when {
+            financeData.contains("+") -> INCOME
+            financeData.contains("-") -> EXPENSE
+            else -> throw IllegalArgumentException("Unknown finance type")
+        }
+    }
 }
 
 @Component
-class GetIncomesHandler(
+class GetFinancesHandler(
     private val telegramService: TelegramService,
-    private val financeService: FinanceService,
+    private val financeRequestNatsService: FinanceRequestNatsService,
     private val textService: TextService,
     private val userSessionService: UserSessionService,
 ) : UserRequestHandler {
 
     override fun isApplicable(request: UpdateRequest): Boolean {
-        return isCommand(request.update, GET_INCOMES.command)
+        return isCommand(request.update, GET_INCOMES.command, GET_EXPENSES.command)
     }
 
     override fun handle(dispatchRequest: UpdateRequest) {
-        financeService.getAllIncomesByUserId(dispatchRequest.chatId)
+        financeRequestNatsService.requestToGetAllFinancesByUserId(
+            dispatchRequest.chatId,
+            dispatchRequest.update.callbackQuery.data.checkCommandIncomeOrExpense()
+        )
+            .map { it.toFinanceResponse() }
             .forEach {
                 telegramService.sendMessage(
                     chatId = dispatchRequest.chatId,
@@ -133,4 +150,14 @@ class GetIncomesHandler(
         }
         userSessionService.saveSession(dispatchRequest.chatId, session)
     }
+
+    private fun String.checkCommandIncomeOrExpense(): Finance {
+        return when (this) {
+            GET_INCOMES.command -> INCOME
+            GET_EXPENSES.command -> EXPENSE
+            else -> throw IllegalArgumentException("Unknown finance type")
+        }
+    }
+
 }
+
