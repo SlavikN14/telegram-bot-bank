f64800a928bc6ce4b1e709066da071dc560ab3e3
slavikn <slavir186@gmail.com>
2023-10-18 12:52:07 +0300

    change gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,32 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val telegramBotVersion = "6.8.0"
-
 plugins {
-    id("org.springframework.boot") version "3.1.3"
-    id("io.spring.dependency-management") version "1.1.3"
-    id("io.gitlab.arturbosch.detekt") version "1.23.1"
     kotlin("jvm") version "1.9.0"
-    kotlin("plugin.spring") version "1.9.0"
-    kotlin("plugin.jpa") version "1.9.0"
-    kotlin("plugin.allopen") version "1.9.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.1"
 }
 
-group = "com.ajaxproject"
-version = "0.0.1-SNAPSHOT"
+allprojects {
+    group = "com.ajaxproject"
+    version = "0.0.1-SNAPSHOT"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-}
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation("org.telegram:telegrambots:$telegramBotVersion")
-    implementation("org.telegram:telegrambotsextensions:$telegramBotVersion")
-    implementation("org.telegram:telegrambots-spring-boot-starter:$telegramBotVersion")
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+    repositories {
+        mavenCentral()
     }
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+subprojects {
+
+    apply(plugin = "kotlin")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = "17"
+        }
+    }
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_17
+    }
 }
---
+++ finance-service-api/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.google.protobuf") version "0.9.4"
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("plugin.spring") version "1.9.0"
+    kotlin("plugin.allopen") version "1.9.0"
+}
+
+dependencies {
+    implementation(project(":internal-api"))
+
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("io.nats:jnats:2.16.14")
+    implementation("com.google.protobuf:protobuf-java:3.24.3")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    testImplementation("com.willowtreeapps.assertk:assertk:0.27.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
