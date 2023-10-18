24b1f86f9b94cf8ba56170e3562047908de0ce66
slavikn <slavir186@gmail.com>
2023-10-18 13:11:33 +0300

    create request service for nats

---
+++ telegram-bot-api/src/main/kotlin/com/ajaxproject/telegrambot/bot/service/FinanceRequestNatsService.kt
@@ -0,0 +1,91 @@
+package com.ajaxproject.telegrambot.bot.service
+
+import com.ajaxproject.internalapi.NatsSubject
+import com.ajaxproject.internalapi.finance.input.reqreply.GetCurrentBalanceRequest
+import com.ajaxproject.internalapi.finance.input.reqreply.GetCurrentBalanceResponse
+import com.ajaxproject.financeservice.dto.toMongoFinance
+import com.ajaxproject.financeservice.dto.toProtoEnumFinance
+import com.ajaxproject.financeservice.dto.toProtoFinance
+import com.ajaxproject.financeservice.enums.Finance
+import com.ajaxproject.financeservice.models.MongoFinance
+import com.ajaxproject.internalapi.finance.input.reqreply.CreateFinanceRequest
+import com.ajaxproject.internalapi.finance.input.reqreply.DeleteFinanceByIdRequest
+import com.ajaxproject.internalapi.finance.input.reqreply.GetAllFinancesByIdRequest
+import com.ajaxproject.internalapi.finance.input.reqreply.GetAllFinancesByIdResponse
+import com.google.protobuf.GeneratedMessageV3
+import com.google.protobuf.Parser
+import io.nats.client.Connection
+import org.bson.types.ObjectId
+import org.springframework.stereotype.Service
+import java.time.Duration
+
+@Service
+class FinanceRequestNatsService(
+    private val connection: Connection,
+) {
+
+    fun requestToGetAllFinancesByUserId(userId: Long, financeType: Finance): List<MongoFinance> {
+        val request: GetAllFinancesByIdRequest = GetAllFinancesByIdRequest.newBuilder()
+            .setUserId(userId)
+            .setFinanceType(financeType.toProtoEnumFinance())
+            .build()
+        val response = doRequest(
+            NatsSubject.FinanceRequest.GET_ALL_FINANCES_BY_ID,
+            request,
+            GetAllFinancesByIdResponse.parser()
+        )
+        return response.success.financeList.map { it.toMongoFinance() }
+    }
+
+    fun requestToCreateFinance(finance: MongoFinance): MongoFinance{
+        val request: CreateFinanceRequest = CreateFinanceRequest.newBuilder()
+            .setFinance(finance.toProtoFinance())
+            .build()
+        val response = doRequest(
+            NatsSubject.FinanceRequest.CREATE_FINANCE,
+            request,
+            CreateFinanceRequest.parser()
+        )
+        return response.finance.toMongoFinance()
+    }
+
+    fun requestToDeleteFinance(id: ObjectId): String {
+        val request: DeleteFinanceByIdRequest = DeleteFinanceByIdRequest.newBuilder()
+            .setId(id.toHexString())
+            .build()
+        val response = doRequest(
+            NatsSubject.FinanceRequest.DELETE_FINANCE,
+            request,
+            DeleteFinanceByIdRequest.parser()
+        )
+        return response.toString()
+    }
+
+    fun requestToGetCurrentBalance(userId: Long): Double {
+        val request: GetCurrentBalanceRequest = GetCurrentBalanceRequest.newBuilder()
+            .setUserId(userId)
+            .build()
+        val response = doRequest(
+            NatsSubject.FinanceRequest.GET_CURRENT_BALANCE,
+            request,
+            GetCurrentBalanceResponse.parser()
+        )
+        return response.success.balance
+    }
+
+    private fun <RequestT : GeneratedMessageV3, ResponseT : GeneratedMessageV3> doRequest(
+        subject: String,
+        payload: RequestT,
+        parser: Parser<ResponseT>,
+    ): ResponseT {
+        val response = connection.requestWithTimeout(
+            subject,
+            payload.toByteArray(),
+            Duration.ofSeconds(NATS_TIMEOUT)
+        )
+        return parser.parseFrom(response.get().data)
+    }
+    companion object{
+        private const val NATS_TIMEOUT = 10L
+    }
+}
