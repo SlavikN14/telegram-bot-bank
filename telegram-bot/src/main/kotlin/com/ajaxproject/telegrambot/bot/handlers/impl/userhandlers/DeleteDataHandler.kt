65000cc63efd025ac8a677d07d80785d15b98259
slavikn <slavir186@gmail.com>
2023-10-28 21:13:05 +0300

    add delete data handler

---
+++ telegram-bot/src/main/kotlin/com/ajaxproject/telegrambot/bot/handlers/impl/userhandlers/DeleteDataHandler.kt
@@ -0,0 +1,43 @@
+package com.ajaxproject.telegrambot.bot.handlers.impl.userhandlers
+
+import com.ajaxproject.telegrambot.bot.beanpostprocessor.annotations.BackToMainMenu
+import com.ajaxproject.telegrambot.bot.beanpostprocessor.annotations.BackToMainMenuCommand
+import com.ajaxproject.telegrambot.bot.enums.Commands.DELETE_DATA
+import com.ajaxproject.telegrambot.bot.enums.TextPropertyName.DATA_IS_DELETED
+import com.ajaxproject.telegrambot.bot.handlers.UserRequestHandler
+import com.ajaxproject.telegrambot.bot.service.FinanceRequestNatsService
+import com.ajaxproject.telegrambot.bot.service.TelegramService
+import com.ajaxproject.telegrambot.bot.service.TextService
+import com.ajaxproject.telegrambot.bot.service.UserService
+import com.ajaxproject.telegrambot.bot.service.updatemodels.UpdateRequest
+import org.springframework.stereotype.Component
+
+@Component
+@BackToMainMenu
+class DeleteDataHandler(
+    private val telegramService: TelegramService,
+    private val userService: UserService,
+    private val financeRequestNatsService: FinanceRequestNatsService,
+    private val textService: TextService,
+) : UserRequestHandler {
+
+    override fun isApplicable(request: UpdateRequest): Boolean {
+        return isCommand(request.update, DELETE_DATA.command)
+    }
+
+    @BackToMainMenuCommand
+    override fun handle(dispatchRequest: UpdateRequest) {
+        val chatId = dispatchRequest.chatId
+
+        userService.deleteUserById(chatId)
+            .subscribe()
+
+        financeRequestNatsService.requestToRemoveAllFinances(chatId)
+            .subscribe()
+
+        telegramService.sendMessage(
+            chatId = chatId,
+            text = textService.readText(DATA_IS_DELETED.name)
+        )
+    }
+}
