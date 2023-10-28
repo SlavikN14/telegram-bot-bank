237a11a7d5a2a2fc72bc76de87ba89c2be489731
slavikn <slavir186@gmail.com>
2023-10-28 21:09:42 +0300

    rewrite user service and repository to reactive approach

--- telegram-bot/src/main/kotlin/com/ajaxproject/telegrambot/bot/repository/UserRepositoryImpl.kt
+++ telegram-bot/src/main/kotlin/com/ajaxproject/telegrambot/bot/repository/UserRepositoryImpl.kt
@@ -1,33 +1,35 @@
 package com.ajaxproject.telegrambot.bot.repository
 
 import com.ajaxproject.telegrambot.bot.models.MongoUser
-import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Repository
 class UserRepositoryImpl(
-    private val mongoTemplate: MongoTemplate,
+    private val mongoTemplate: ReactiveMongoTemplate,
 ) : UserRepository {
 
-    override fun findByPhoneNumber(number: String): MongoUser? {
+    override fun findByPhoneNumber(number: String): Mono<MongoUser?> {
         return mongoTemplate.findOne(
             Query(Criteria.where("phoneNumber").`is`(number)),
             MongoUser::class.java
         )
     }
 
-    override fun findAll(): List<MongoUser> {
+    override fun findAll(): Flux<MongoUser> {
         return mongoTemplate.findAll(MongoUser::class.java)
     }
 
-    override fun save(user: MongoUser): MongoUser {
+    override fun save(user: MongoUser): Mono<MongoUser> {
         return mongoTemplate.save(user)
     }
 
-    override fun deleteById(userId: Long) {
-        mongoTemplate.findAndRemove(
+    override fun deleteById(userId: Long): Mono<MongoUser> {
+        return mongoTemplate.findAndRemove(
             Query(Criteria.where("id").`is`(userId)),
             MongoUser::class.java
         )
