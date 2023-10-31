4994762fc1a32d6174cab0113a77dd6ec05b0956
slavikn <slavir186@gmail.com>
2023-10-31 16:15:13 +0200

    rewrite texts reader service

--- telegram-bot/src/main/kotlin/com/ajaxproject/telegrambot/bot/service/TextService.kt
+++ telegram-bot/src/main/kotlin/com/ajaxproject/telegrambot/bot/service/TextService.kt
@@ -1,26 +1,41 @@
 package com.ajaxproject.telegrambot.bot.service
 
+import com.ajaxproject.telegrambot.bot.enums.Localization
+import jakarta.annotation.PostConstruct
 import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.objects.Update
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 import java.nio.charset.StandardCharsets
 import java.util.Properties
 
 @Service
 class TextService {
 
-    lateinit var pathResource: String
+    val textMap: MutableMap<Localization, Map<String, String>> = mutableMapOf()
 
-    fun readText(property: String): String {
-        return ClassPathResource(pathResource).inputStream
-            .use { Properties().apply {
-                    it.reader(StandardCharsets.UTF_8).use { reader ->
-                        load(reader)
+    @PostConstruct
+    fun readTextFromFile() {
+        Flux.fromIterable(Localization.entries)
+            .flatMap { localization ->
+                Mono.fromSupplier {
+                    val properties = ClassPathResource(localization.path).inputStream.use {
+                        Properties().apply {
+                            it.reader(StandardCharsets.UTF_8).use { reader -> load(reader) }
+                        }
                     }
+                    localization to properties.entries.associate { it.key.toString() to it.value.toString() }
                 }
             }
-            .getProperty(property)
-            ?: throw FileNameNotFoundException("File $pathResource not found")
+            .collectMap({ it.first }, { it.second })
+            .doOnNext { resultMap ->
+                textMap.putAll(resultMap)
+            }
+            .subscribeOn(Schedulers.boundedElastic())
+            .onErrorMap { FileNameNotFoundException("File not found for some Localization") }
+            .subscribe()
     }
 }
 
