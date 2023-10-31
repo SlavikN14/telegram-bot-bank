e4c1bc63702144578e6b5dfb5fbcb31672a3d35b
slavikn <slavir186@gmail.com>
2023-10-31 16:11:14 +0200

    remove unnecessary beanPostProcessor

--- telegram-bot/src/main/kotlin/com/ajaxproject/telegrambot/bot/beanpostprocessor/annotations/BackToMainMenuCommand.kt
+++
@@ -1,5 +0,0 @@
-package com.ajaxproject.telegrambot.bot.beanpostprocessor.annotations
-
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FUNCTION)
-annotation class BackToMainMenuCommand
