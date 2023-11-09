c96d520e7e7f3e7fbf8bae291d10ce90509e62b4
slavikn <slavir186@gmail.com>
2023-11-09 16:20:44 +0200

    change build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,17 @@ subprojects {
     apply(plugin = "kotlin")
     apply(plugin = "io.gitlab.arturbosch.detekt")
 
+    dependencies {
+        implementation("io.projectreactor:reactor-core:3.5.11")
+
+        implementation("io.grpc:grpc-protobuf:1.58.0")
+        implementation("io.grpc:grpc-netty:1.58.0")
+        implementation("io.grpc:grpc-stub:1.58.0")
+        implementation("com.salesforce.servicelibs:reactor-grpc:1.2.4")
+        implementation("com.salesforce.servicelibs:reactive-grpc-common:1.2.4")
+        implementation("com.salesforce.servicelibs:reactor-grpc-stub:1.2.4")
+    }
+
     tasks.withType<KotlinCompile> {
         kotlinOptions {
             freeCompilerArgs += "-Xjsr305=strict"
