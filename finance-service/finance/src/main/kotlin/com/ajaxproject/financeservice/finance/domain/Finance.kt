be0f3307ea830cd66c169803d1e6c3f226f98c94
slavikn <slavir186@gmail.com>
2023-11-09 22:19:31 +0200

    create domain entity

---
+++ finance-service/finance/src/main/kotlin/com/ajaxproject/financeservice/finance/domain/Finance.kt
@@ -0,0 +1,13 @@
+package com.ajaxproject.financeservice.finance.domain
+
+import java.util.Date
+
+data class Finance(
+
+    val id: String? = null,
+    val userId: Long,
+    val financeType: String,
+    val amount: Double,
+    val description: String,
+    val date: Date,
+)
